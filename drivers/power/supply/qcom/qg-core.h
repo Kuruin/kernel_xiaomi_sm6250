fe87fd07ecae7357c4138158d3e9809e97d27cff
Anirudh Ghayal <aghayal@codeaurora.org>
2020-01-03 11:36:26 +0530

    power: qpnp-qg: Add support for batt-soc based SOC scaling (BASS)
    At cold temperatures, when the load on battery suddenly drops
    there is a possibility of monotonic-SOC being stuck for a long
    time due to a rise in system SOC. This leads to a bad UX.
    
    Fix this by gradually dropping the monotonic-SOC if the
    battery-SOC has dropped by 1%, irrespective of the increase
    in system SOC.
    
    Change-Id: I5da1cf368d523768d0b267b314394612934a620e
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/supply/qcom/qg-core.h
+++ drivers/power/supply/qcom/qg-core.h
@@ -83,6 +83,7 @@ struct qg_dt {
 	bool			fvss_enable;
 	bool			multi_profile_load;
 	bool			tcss_enable;
+	bool			bass_enable;
 };
 
 struct qg_esr_data {
@@ -180,6 +181,7 @@ struct qpnp_qg {
 	bool			fvss_active;
 	bool			fastcharge_mode_enabled;
 	bool			tcss_active;
+	bool			bass_active;
 	int			charge_status;
 	int			charge_type;
 	int			chg_iterm_ma;
@@ -196,6 +198,7 @@ struct qpnp_qg {
 	int			soc_tcss;
 	int			tcss_entry_count;
 	int			max_fcc_limit_ma;
+	int			bsoc_bass_entry;
 	u32			fifo_done_count;
 	u32			wa_flags;
 	u32			seq_no;
