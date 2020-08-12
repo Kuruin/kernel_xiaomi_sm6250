151015d7b1699841156918a6eecf9043b1d4120b
Volodymyr Zhdanov <wight554@gmail.com>
2020-08-12 18:25:32 +0300

    Revert "power: qcom: add support for battery verification chip"
    This reverts commit 1177c2d98830898ce6951e7d515abcbb8cb2bd86.

--- drivers/power/supply/qcom/qg-core.h
+++ drivers/power/supply/qcom/qg-core.h
@@ -79,9 +79,6 @@ struct qg_dt {
 	bool			multi_profile_load;
 	bool			tcss_enable;
 	bool			bass_enable;
-
-	/* battery verification */
-	bool			batt_verify_enable;
 };
 
 struct qg_esr_data {
@@ -206,11 +203,6 @@ struct qpnp_qg {
 	struct cycle_counter	*counter;
 	/* ttf */
 	struct ttf		*ttf;
-
-	/* battery verification */
-	struct power_supply *max_verify_psy;
-	struct delayed_work profile_load_work;
-	bool				profile_judge_done;
 };
 
 struct ocv_all {
